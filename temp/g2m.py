c1f763abfda5f40f4a3dddbc5e905416c9b8eb34
Claudio Rego <claudiojmrego@gmail.com>
2017-03-11 23:58:39 +0000

    gpio 2 max test

---
+++ temp/g2m.py
@@ -0,0 +1,14 @@
+import OSC
+
+c = OSC.OSCClient()
+c.connect(('localhost', 5008))   # connect to SuperCollider
+oscmsg = OSC.OSCMessage()
+
+oscmsg.setAddress("/foo")
+oscmsg.append(1000)
+c.send(oscmsg)
+
+oscmsg.clear()
+oscmsg.setAddress("/bar")
+oscmsg.append(999)
+c.send(oscmsg)
