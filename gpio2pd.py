78fcdcb1c1e9e26ed2c72bffebdfe3e596a714d0
Claudio Rego <claudiojmrego@gmail.com>
2017-03-11 18:31:29 +0000

    cleaned up some code

--- gpio2pd.py
+++ gpio2pd.py
@@ -41,6 +41,8 @@ def RCtime (PiPin):
   return str(end - start)
 
 # Connects the socket
+# DGRAM=UDP, STREAM=TCP
+# (Max/MSP is only compatible with UDP)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 def socketConnect ():
     try:
@@ -56,7 +58,9 @@ try:
   while True:
     # Measure timing using GPIO4
     risetime = RCtime(4)
-    # Send to connected socket
+    # Send to the connected socket
+    # (as we're using UDP, we must
+    # send separate messages)
     s.sendall('foo %s%s' % (n, EOF))
     s.sendall('bar %s%s' % (risetime, EOF))
     # Advance counter
